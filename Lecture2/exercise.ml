e300141b9a390dc4199207298b23a24d50b3214a
Daniel Johansen <dajoh16@student.sdu.dk>
2020-02-11 08:20:39 +0100

    Exercises Lecture 2

--- Lecture2/exercise.ml
+++ Lecture2/exercise.ml
@@ -1,7 +1,46 @@
+open QCheck
+
 let rec msb n = 
   if n = 0
   then 0
   else 1 + msb(n lsr 1);;
 
+let rec length l = match l with
+  | [] -> 0
+  | elem::elems -> 1 + length(elems)
 
+let rec sum l = match l with
+  | [] -> 0
+  | elem::elems -> elem + sum(elems)
 
+let rec member l num = match l with 
+  | [] -> false
+  | elem::elems -> if elem = num
+    then true
+    else member elems num
+
+let fst n = match n with
+    (x,y) -> x
+
+let snd n = match n with
+    (x,y) -> y
+
+let sum_test = Test.make ~name:"Sum Test" ~count:100000 (pair (list int) (list int)) (fun (xs,ys) -> sum (xs@ys) = sum xs + sum ys)
+
+let rec merge l1 l2 = match l1, l2 with
+  |  [],list -> l2
+  | list,[] -> l1
+  | elem1::elems1, elem2::elems2 -> if elem1 <= elem2 
+    then elem1 :: merge elems1 (elem2::elems2)
+    else elem2 :: merge (elem1::elems1) elems2 
+
+let merge_test = Test.make ~name:"Merge Test" ~count:1000 (pair (list int) (list int)) (
+    fun (xs,ys) -> merge (List.sort compare xs) (List.sort compare ys) = List.sort compare (xs@ys) ) 
+
+let _ = QCheck_runner.run_tests ~verbose:true [merge_test]
+
+let test1 = Test.make int (fun x -> x = Int64.to_int(Int64.of_int x))
+let test2 = Test.make int (fun x -> x = Int32.to_int(Int32.of_int x))
+let test3 = Test.make int (fun x -> x = int_of_string(string_of_int x))
+
+let _ = QCheck_runner.run_tests ~verbose:true [test1;test2;test3]
