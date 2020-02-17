533bfa76e2a4200a8e2550b98ce9d7dacac2cfb2
Daniel Johansen <dajoh16@student.sdu.dk>
2020-02-17 14:54:39 +0100

    Interpreter Exercise

--- Lecture3/exercise.ml
+++ Lecture3/exercise.ml
@@ -47,3 +47,41 @@ let rec size ae = match ae with
     let s1 = size ae1 in
     s0 + s1 + 1
 
+(* a datatype of abstract machine instructions *)
+type inst =
+  | Load
+  | Push of int
+  | Add
+  | Mult
+
+(* our compiler from arithmetic expressions to instructions *)
+let rec compile ae = match ae with
+  | X -> [Load]
+  | Lit i -> [Push i]
+  | Plus (ae0, ae1) ->
+    let is0 = compile ae0 in
+    let is1 = compile ae1 in
+    is0 @ is1 @ [Add]
+  | Times (ae0, ae1) ->
+    let is0 = compile ae0 in
+    let is1 = compile ae1 in
+    is0 @ is1 @ [Mult]
+
+exception Cannot_add_stack_with_1_entry
+exception Cannot_multiply_stack_with_1_entry
+
+let rec run xreg inst stack = match inst with
+  | [] -> stack
+  | e::elems -> run xreg elems (match e with 
+      | Load -> [xreg] @ stack   
+      | Push i -> [i] @ stack
+      | Add -> (match stack with 
+          | [] -> []
+          | e1::stackElems -> (match stackElems with
+              | [] -> raise Cannot_add_stack_with_1_entry
+              | e2::remainder -> ([(e1+e2)] @ remainder)))
+      | Mult -> (match stack with 
+          | [] -> []
+          | e1::stackElems -> (match stackElems with
+              | [] -> raise Cannot_multiply_stack_with_1_entry
+              | e2::remainder -> ([(e1 * e2)] @ remainder)))) 
